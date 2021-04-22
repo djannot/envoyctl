9038d72e9a96e983c4c013519660f5f50f521308
Denis JANNOT <denisjannot@macbook-pro-de-denis.home>
2021-04-22 14:23:25 +0200

    Adding support for config dumps with include_eds

--- envoyctl.js
+++ envoyctl.js
@@ -54,47 +54,59 @@ function array_match(arr, expr) {
 }
 
 function parse(str) {
-        let js = JSON.parse(str)
+	let js = JSON.parse(str)
 
-        let configs = {};
-        js.configs.forEach(config => {
-                configs[config['@type']] = config;
-        });
+	let configs = {};
+	js.configs.forEach(config => {
+			configs[config['@type']] = config;
+	});
 
-        routes_by_dynamic_listeners = {};
-        let listeners_config_dump = configs['type.googleapis.com/envoy.admin.v3.ListenersConfigDump'];
-        listeners_config_dump.dynamic_listeners.forEach(dynamic_listener => {
-                routes_by_dynamic_listeners[dynamic_listener.name] = [];
-                dynamic_listener.active_state.listener.filter_chains.forEach(filter_chain => {
-                        filter_chain.filters.forEach(filter => {
-                                if ((filter.name == 'envoy.filters.network.http_connection_manager') && (filter.typed_config.rds)) {
+	routes_by_dynamic_listeners = {};
+	let listeners_config_dump = configs['type.googleapis.com/envoy.admin.v3.ListenersConfigDump'];
+	listeners_config_dump.dynamic_listeners.forEach(dynamic_listener => {
+		routes_by_dynamic_listeners[dynamic_listener.name] = [];
+		dynamic_listener.active_state.listener.filter_chains.forEach(filter_chain => {
+			filter_chain.filters.forEach(filter => {
+				if ((filter.name == 'envoy.filters.network.http_connection_manager') && (filter.typed_config.rds)) {
 					if ((typeof argv.r === 'undefined') || (filter.typed_config.rds.route_config_name == argv.r)) {
-                                        	routes_by_dynamic_listeners[dynamic_listener.name].push(filter.typed_config.rds.route_config_name);
+											routes_by_dynamic_listeners[dynamic_listener.name].push(filter.typed_config.rds.route_config_name);
 					}
-                                }
-                        });
-                });
-        });
+				}
+			});
+		});
+	});
 
-        route_configs_by_route = {}
-        let routes_config_dump = configs['type.googleapis.com/envoy.admin.v3.RoutesConfigDump'];
-        routes_config_dump.dynamic_route_configs.forEach(dynamic_route_config => {
-                route_configs_by_route[dynamic_route_config.route_config.name] = dynamic_route_config.route_config;
-        });
+	route_configs_by_route = {}
+	let routes_config_dump = configs['type.googleapis.com/envoy.admin.v3.RoutesConfigDump'];
+	routes_config_dump.dynamic_route_configs.forEach(dynamic_route_config => {
+			route_configs_by_route[dynamic_route_config.route_config.name] = dynamic_route_config.route_config;
+	});
 
-        Object.keys(routes_by_dynamic_listeners).forEach(dynamic_listener => {
-                routes_by_dynamic_listeners[dynamic_listener].forEach(route => {
-                        route_configs_by_route[route].virtual_hosts.forEach(virtual_host => {
+	endpoint_config_by_route = {};
+	if('type.googleapis.com/envoy.admin.v3.EndpointsConfigDump' in configs) {
+		let endpoints_config_dump = configs['type.googleapis.com/envoy.admin.v3.EndpointsConfigDump'];
+		endpoints_config_dump.dynamic_endpoint_configs.forEach(dynamic_endpoint_config => {
+			endpoint_config_by_route[dynamic_endpoint_config.endpoint_config.cluster_name.replace(/_\./g, "|")] = dynamic_endpoint_config.endpoint_config;
+		});
+	}
+
+	Object.keys(routes_by_dynamic_listeners).forEach(dynamic_listener => {
+		routes_by_dynamic_listeners[dynamic_listener].forEach(route => {
+			route_configs_by_route[route].virtual_hosts.forEach(virtual_host => {
 				if ((typeof argv.d === 'undefined') || (array_match(virtual_host.domains, argv.d))) {
-                        	        if (virtual_host.routes) {
+					if (virtual_host.routes) {
 						virtual_host.routes.forEach(r => {
-                                                	table_data.push([dynamic_listener, route, virtual_host.domains.join("\n"), JSON.stringify(r.match, null, 2), JSON.stringify(r.route, null, 2)]);
-                                        	});
+							if('cluster' in r.route && r.route.cluster in endpoint_config_by_route) {
+								table_data.push([dynamic_listener, route, virtual_host.domains.join("\n"), JSON.stringify(r.match, null, 2), JSON.stringify(r.route, null, 2), JSON.stringify(endpoint_config_by_route[r.route.cluster], null, 2)]);
+							} else {
+								table_data.push([dynamic_listener, route, virtual_host.domains.join("\n"), JSON.stringify(r.match, null, 2), JSON.stringify(r.route, null, 2)]);
+							}
+						});
 					}
-                                }
-                        });
-                });
-        });
+				}
+			});
+		});
+	});
 
-        console.table(table_data.toString());
+	console.table(table_data.toString());
 }
